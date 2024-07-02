aa6a9bcc8fa0e0ba2115b0ff81d9607ceb7c600d
Stian Gaustad <stian.k.gaustad@gmail.com>
2024-07-02 16:15:09 +0200

    Program - Added FetchingService and used appsettings token

--- CurrencyConverterAPI/Program.cs
+++ CurrencyConverterAPI/Program.cs
@@ -1,6 +1,13 @@
+using CurrencyConverterAPI.Services;
+
 var builder = WebApplication.CreateBuilder(args);
+FetchingService updater = new FetchingService(builder.Configuration.GetConnectionString("fixerToken")!);
+
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
+
+await updater.UpdateNow();
 
 app.Run();
