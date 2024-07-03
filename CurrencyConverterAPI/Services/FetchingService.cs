f8d17661d4f226f96f55cfc4998020a1c601fd38
Stian Gaustad <stian.gaustad@no.experis.com>
2024-07-03 12:18:54 +0200

    FetchingService - Added Parsing of Json from external API and insertion into database

--- CurrencyConverterAPI/Services/FetchingService.cs
+++ CurrencyConverterAPI/Services/FetchingService.cs
@@ -1,17 +1,24 @@
-﻿namespace CurrencyConverterAPI.Services
+﻿using CurrencyConverterAPI.Models;
+using CurrencyConverterAPI.Repository;
+using System.Globalization;
+using System.Text.Json;
+
+namespace CurrencyConverterAPI.Services
 {
     public class FetchingService
     {
         private Timer _timer;
         private HttpClient _httpClient;
         private string _apiUrl;
+        private CurrencyRepository _currencyRepo;
 
-        public FetchingService (String token) 
+        public FetchingService (String token, CurrencyRepository currencyRepo) 
         {
             // Fixer documentation for accessing data https://fixer.io/documentation
             // Cant use https with free tier
             _apiUrl = "http://data.fixer.io/api/latest?access_key=" + token + "&format=1";
             _httpClient = new HttpClient();
+            _currencyRepo = currencyRepo;
             //_timer = new Timer(null, null, 0, 24*60*60);
         }
 
@@ -22,9 +29,42 @@
 
         private async Task FetchData() 
         {
-            var response = await _httpClient.GetAsync(_apiUrl);
-            var content = response.Content.ReadAsStringAsync();
-            Console.WriteLine(content);
+            try
+            {
+                var response = await _httpClient.GetAsync(_apiUrl);
+                response.EnsureSuccessStatusCode();
+
+                // Read the content of response
+                string contentString = await response.Content.ReadAsStringAsync();
+                // Convert from json string to usable objects
+                var jsonData = JsonDocument.Parse(contentString);
+                JsonElement currencyRates = jsonData.RootElement.GetProperty("rates");
+
+                List<Currency> convertedCurrencies = currencyRates.EnumerateObject()
+                    .Select(c =>
+                        new Currency() {
+                            CurrencyCode = c.Name,
+                            ExchangeRate = decimal.Parse(
+                                c.Value.ToString(),
+                                NumberStyles.Float | NumberStyles.AllowExponent,
+                                CultureInfo.InvariantCulture
+                            )
+                        }
+                    )
+                    .ToList();
+
+                await _currencyRepo.UpdateCurrencyExchange(convertedCurrencies);
+            }
+            catch (HttpRequestException e)
+            {
+                // TODO: Add propper logger
+                Console.WriteLine(e);
+            }
+            catch (JsonException e) 
+            {
+                // TODO: Add proper logger
+                Console.WriteLine(e);
+            }
         }
     }
 }
