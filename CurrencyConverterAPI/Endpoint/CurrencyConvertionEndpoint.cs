bc402c998e073c60fad3ac0dc5b0a2a2bdd787b8
Stian Gaustad <stian.gaustad@no.experis.com>
2024-07-03 12:19:42 +0200

    CurrencyConvertionEndpoint - Added dedicated endpoint for handling convertion between currencies

---
+++ CurrencyConverterAPI/Endpoint/CurrencyConvertionEndpoint.cs
@@ -0,0 +1,29 @@
+﻿using Azure.Core;
+using CurrencyConverterAPI.Models;
+using CurrencyConverterAPI.Repository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CurrencyConverterAPI.Endpoint
+{
+    public static class CurrencyConvertionEndpoint
+    {
+        public static void CurrencyConvertionEndpointConfiguration(this WebApplication app) 
+        {
+            app.MapPost("/", CalculateAmount);
+        }
+
+        public static async Task<IResult> CalculateAmount(CurrencyRepository repo, [FromQuery] ConvertionRequest request)
+        {
+            Currency? Cur1 = await repo.GetCurrency(request.CurrencyCode1);
+            Currency? Cur2 = await repo.GetCurrency(request.CurrencyCode2);
+
+            if (Cur1 is null || Cur2 is null)
+            {
+                return TypedResults.BadRequest("Invalid currency codes");
+            }
+
+            decimal ConvertedCurrencyAmount = request.CurrencyAmount * (Cur2.ExchangeRate / Cur1.ExchangeRate);
+            return TypedResults.Ok(ConvertedCurrencyAmount);
+        }
+    }
+}
