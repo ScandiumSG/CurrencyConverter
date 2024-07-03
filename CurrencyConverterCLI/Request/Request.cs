96a3c5815df39803a4903d64696fa685ef3f4e02
Stian Gaustad <stian.gaustad@no.experis.com>
2024-07-03 13:13:30 +0200

    CLI - Request object added to track currency and amount

---
+++ CurrencyConverterCLI/Request/Request.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CurrencyConverterCLI.Request
+{
+
+    public class Request
+    {
+        private string _currencyOne;
+        private string _currencyTwo;
+        private decimal _currencyAmount;
+
+        public string CurrencyOne { get; set; }
+
+        public string CurrencyTwo { get; set; }
+
+        public decimal CurrencyAmount { 
+            get { return _currencyAmount; } 
+            set { _currencyAmount = value; } 
+        }
+
+        public FormUrlEncodedContent GetPostBody() 
+        {
+            var values = new Dictionary<string, string>
+            {
+                { "CurrencyCode1", CurrencyOne},
+                { "CurrencyCode2", CurrencyTwo},
+                { "CurrencyAmount", CurrencyAmount.ToString()}
+            };
+
+            return new FormUrlEncodedContent(values);
+        }
+    }
+
+}
