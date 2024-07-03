5ef3bae1b2328480e1be0338aea933ee871a5c25
Stian Gaustad <stian.gaustad@no.experis.com>
2024-07-03 10:56:31 +0200

    Db - Added data context and connection to Cosmos DB in program

--- CurrencyConverterAPI/Program.cs
+++ CurrencyConverterAPI/Program.cs
@@ -1,13 +1,20 @@
+using CurrencyConverterAPI.Data;
 using CurrencyConverterAPI.Services;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 FetchingService updater = new FetchingService(builder.Configuration.GetConnectionString("fixerToken")!);
+
+builder.Services.AddDbContext<DataContext>
+    (
+        opt => opt.UseCosmos("https://localhost:8081", "C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw==", databaseName: "currencyDb")
+    );
 
 
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
 
-await updater.UpdateNow();
+app.MapGet("/update", async () => await updater.UpdateNow());
 
 app.Run();
