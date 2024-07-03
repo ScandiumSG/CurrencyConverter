fe29e1fedd0d1965738fb9a941a0fe7d58863d53
Stian Gaustad <stian.gaustad@no.experis.com>
2024-07-03 12:18:17 +0200

    ConvertionRequest - Added a post DTO model for converting currency

---
+++ CurrencyConverterAPI/Models/ConvertionRequest.cs
@@ -0,0 +1,20 @@
+﻿namespace CurrencyConverterAPI.Models
+{
+    public class ConvertionRequest
+    {
+        /// <summary>
+        /// The currency code to convert from. Must be a string of 3-characters
+        /// </summary>
+        public string CurrencyCode1 { get; set; }
+
+        /// <summary>
+        /// The currency code to convert to. Must be a string of 3-characters
+        /// </summary>
+        public string CurrencyCode2 { get; set; }
+
+        /// <summary>
+        /// The amount of CurrencyCode1 to convert into CurrencyCode2
+        /// </summary>
+        public decimal CurrencyAmount { get; set; }
+    }
+}
