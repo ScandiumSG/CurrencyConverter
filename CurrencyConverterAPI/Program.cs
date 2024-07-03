cf7f5fd4c27ea537acbff046eab755ca7356200b
Stian Gaustad <stian.gaustad@no.experis.com>
2024-07-03 14:32:55 +0200

    API - Added health check

--- CurrencyConverterAPI/Program.cs
+++ CurrencyConverterAPI/Program.cs
@@ -1,16 +1,14 @@
 using CurrencyConverterAPI.Data;
 using CurrencyConverterAPI.Endpoint;
-using CurrencyConverterAPI.Models;
 using CurrencyConverterAPI.Repository;
 using CurrencyConverterAPI.Services;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<DataContext>
     (
-        opt => opt.UseCosmos("https://localhost:8081", "C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw==", databaseName: "currencyDb")
+        opt => opt.UseCosmos("https://localhost:8081", "C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw==", databaseName: "CosmosDb")
     );
 
 builder.Services.AddScoped<CurrencyRepository, CurrencyRepository>();
@@ -23,9 +21,8 @@ builder.Services.AddScoped<FetchingService>(provider =>
 });
 
 var app = builder.Build();
-app.UseHttpsRedirection();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/health", () => "Server running.");
 app.MapGet("/update", async (FetchingService updater) => await updater.UpdateNow());
 
 app.CurrencyConvertionEndpointConfiguration();
