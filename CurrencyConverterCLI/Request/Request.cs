bb92dbca7662485762a8b562dbbec1537a206cf0
Stian Gaustad <stian.gaustad@no.experis.com>
2024-07-03 15:26:12 +0200

    CLI - Added validation of Request input

--- CurrencyConverterCLI/Request/Request.cs
+++ CurrencyConverterCLI/Request/Request.cs
@@ -1,27 +1,35 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Globalization;
 
 namespace CurrencyConverterCLI.Request
 {
 
     public class Request
     {
-        private string _currencyOne;
-        private string _currencyTwo;
-        private decimal _currencyAmount;
+        private string _currencyOne = "";
+        private string _currencyTwo = "";
+        private decimal _currencyAmount = 0;
 
-        public string CurrencyOne { get; set; }
-
-        public string CurrencyTwo { get; set; }
-
-        public decimal CurrencyAmount { 
-            get { return _currencyAmount; } 
-            set { _currencyAmount = value; } 
+        public string CurrencyOne 
+        { 
+            get { return _currencyOne.ToUpper(); } 
+            set {if (ValidateCurrencyCode(value)) { _currencyOne = value; }} 
         }
 
+        public string CurrencyTwo
+        {
+            get { return _currencyTwo.ToUpper(); }
+            set { if (ValidateCurrencyCode(value)) { _currencyTwo = value; } }
+        }
+
+        public decimal CurrencyAmount 
+        { 
+            get { return _currencyAmount; } 
+        }
+
+        /// <summary>
+        /// Transform the request object into a FormUrlEncodedContent object. 
+        /// </summary>
+        /// <returns>FormUrlEncodedContent containing CurrencyCode1, CurrencyCode2, and CurrencyAmount.</returns>
         public FormUrlEncodedContent GetPostBody() 
         {
             var values = new Dictionary<string, string>
@@ -32,6 +40,33 @@ namespace CurrencyConverterCLI.Request
             };
 
             return new FormUrlEncodedContent(values);
+        }
+
+        /// <summary>
+        /// Parse a string into a decimal value. If string was successfully parsed into decimal sets the value of CurrencyAmount for the Request object.
+        /// </summary>
+        /// <param name="value"> The string that is to be attempted to be transformed into a decimal.</param>
+        /// <returns>Boolean, true if the string was successfully parsed into a decimal, false otherwise.</returns>
+        public bool ValidateAndSetDecimal(string value) 
+        {
+            return decimal.TryParse(value, CultureInfo.InvariantCulture, out _currencyAmount);
+        }
+
+        /// <summary>
+        /// Validate valid 3-character currency code. 
+        /// </summary>
+        /// <param name="value">The string to validate</param>
+        /// <returns>Boolean, true if string fits criteria, false otherwise.</returns>
+        private bool ValidateCurrencyCode(string value) 
+        {
+            if (value.Length == 3 && value.All(c => char.IsLetter(c)))
+            {
+                return true;
+            }
+            else 
+            {
+                return false;
+            }
         }
     }
 
